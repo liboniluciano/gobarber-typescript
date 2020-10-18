22cedc79b618f7e65b77c44644525421770e0b94
Luciano Liboni <luciano_liboni@hotmail.com>
2020-10-18 13:27:11 -0300

    appointment model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -26,11 +20,7 @@ appointmentsRouter.post('/', (req, res) => {
   }
 
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
   return res.json(appointment);
