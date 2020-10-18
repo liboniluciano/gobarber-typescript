fc93e477ff61aae6a173bbb68652eaa00412f6a7
Luciano Liboni <luciano_liboni@hotmail.com>
2020-10-18 15:57:33 -0300

    create appointment repository with db

--- src/database/migrations/1603044656042-CreateAppointments.ts
+++ src/database/migrations/1603044656042-CreateAppointments.ts
@@ -11,7 +11,8 @@ export class CreateAppointments1603044656042 implements MigrationInterface {
               name: 'id',
               type: 'varchar',
               isPrimary: true,
-              generationStrategy: "uuid"
+              generationStrategy: "uuid",
+              default: 'uuid_generate_v4()'
             },
             {
               name: 'provider',
