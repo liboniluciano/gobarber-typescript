bc8457932ba1ba5a1743fbdceaef42290eb20a90
Luciano Liboni <luciano_liboni@hotmail.com>
2020-10-18 20:44:26 -0300

    create global handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,37 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { NextFunction, Request, response, Response } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.json({ Ping: 'Pong'});
+  return res.json({ Ping: 'Pong' });
 });
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
 app.listen(3333, () => {
   console.log('Server started on port 3333');
-})
+});
